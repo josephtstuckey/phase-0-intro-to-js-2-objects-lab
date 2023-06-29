7f99863131390a26c8aa59ecadccc2d33e4536b9
Joseph Stuckey <josephtstuckey@gmail.com>
2023-06-29 15:02:59 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+/*  employees
+        updateEmployeeWithKeyAndValue(employee, key, value)
+        1) "before each" hook for "returns an employee with the original key value pairs and the new key value pair"
+
+        destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+        2) updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
+        
+        deleteFromEmployeeByKey(employee, key)
+        3) deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+        4) does not modify the original employee (it is non-destructive)
+
+        destructivelyDeleteFromEmployeeByKey(employee, key)
+        5) returns employee without the deleted key/value pair
+        6) modifies the original employee
+*/
+
+const employee = {
+    name: 'Sherlock',
+    streetAddress: '221b Baker Street'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
